5b9ea1226afd1e53acd6a0dced815c6c07dfafbe
paulovferreira <pvf.ferreira@gmail.com>
2025-01-31 22:24:03 -0300

    PROJETO VITE EXEMPLO

---
+++ PROJETO_VITE/src/App.tsx
@@ -0,0 +1,26 @@
+import './estilo/estilo.css'
+
+function App() {
+
+  return (
+    <div className='app-container'>      
+      <div className='app-div1'>
+        <div className='app-div1-1'></div>
+        <div className='app-div1-2'></div>        
+        <div className='app-div1-3'></div>
+      </div>
+
+      <div className='app-div2'>
+        <div className='app-div2-1'></div>
+        <div className='app-div2-2'></div>
+      </div>
+      
+      <div className='app-div3'>
+        <div className='app-div3-1'></div>
+        <div className='app-div3-2'></div>
+      </div>
+    </div>
+  )
+}
+
+export default App
