da226724a75777d55011b9e457ad8328d3fc1b6a
paulovferreira <pvf.ferreira@gmail.com>
2025-02-21 22:21:09 -0300

    Aula2102

--- PROJETO_VITE/src/pages/Sobre.tsx
+++ PROJETO_VITE/src/pages/Sobre.tsx
@@ -1,11 +1,35 @@
-import Cabecalho from "../Cabecalho";
-import Rodape from "../Rodape";
+import Cabecalho from "../components/cabecalho";
+import Rodape from "../components/rodape";
+import { useState } from "react";
 
 function Sobre(){
+
+    //let numero: number = 10;
+    const [numero, setnumero] = useState(0);
+    function handleClickButton(){        
+        //numero += 10;        
+        setnumero(numero + 10);
+    }
+
+    const [nome, setnome] = useState('');
+    function HandleMudarNome(){
+        setnome('Jhony')
+    }
+
     return(
         <div>
             <Cabecalho />
             Sobre
+            <br />
+            O numero informado é: {numero}
+            <br />
+            <button onClick={handleClickButton}>Somar 10</button>
+            <hr />
+            Nome:
+            <br />
+            O meu nome não é: {nome}
+            <br />
+            <button onClick={HandleMudarNome}>Alterar Nome</button>
             <Rodape />
         </div>
     )
